369b0ae4a45a4ce065a05b85312cbc99b824d551
Jasper Chang <jasperc8@gmail.com>
2018-01-04 23:55:05 +0800

    root reducer

---
+++ src/reducers.js
@@ -0,0 +1,16 @@
+import { combineReducers } from 'redux-immutable';
+import { fromJS } from 'immutable';
+import AppReducer from '@/containers/App/reducer';
+
+/**
+ * Creates the main reducer with the asynchronously loaded ones
+ */
+export default function createReducer(asyncReducers) {
+  return combineReducers({
+    /**
+     * load reducer here
+     */
+    app: AppReducer,
+    ...asyncReducers,
+  });
+}
